456774e88be299e953da517f70c252936d3e0795
Nikita Demchenko <nikitosi3012@gmail.com>
2026-03-31 09:50:36 +0300

    add fps label to main GUI

--- ui/main_window.py
+++ ui/main_window.py
@@ -5,6 +5,7 @@ from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtCore import Qt
 
 from video.worker import VideoWorker
+from utils.fps_counter import FPSCounter
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -15,15 +16,23 @@ class MainWindow(QWidget):
         self.label = QLabel()
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        self.fps_label = QLabel("FPS: 0")
+
         layout = QVBoxLayout()
         layout.addWidget(self.label)
+        layout.addWidget(self.fps_label)
         self.setLayout(layout)
+
+        self.fps_counter = FPSCounter()
 
         self.worker = VideoWorker()
         self.worker.frame_ready.connect(self.update_frame)
         self.worker.start()
 
     def update_frame(self, frame):
+        fps = self.fps_counter.update()
+        self.fps_label.setText(f"FPS: {fps}")
+
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         h, w, ch = rgb.shape
